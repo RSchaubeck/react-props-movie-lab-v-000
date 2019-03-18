07c7a0f276c864daaf0f34b3b5e36e406fc58adb
Richard Schaubeck <richard.schaubeck@gmail.com>
2019-03-18 18:59:17 +0000

    Automatically backed up by Learn

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -12,8 +12,7 @@ export default class CardBack extends Component {
 
   generateRatingElement = () => {
     // implement meeeee! See the readme for instructions
-    if(this.props.IMDBRating) {
-      return(<img src={imgMapper[this.props.IMDBRating]} alt={this.props.IMDBRating} />)
+    this.props.IMDBRating ? <img src={imgMapper[this.props.IMDBRating]} alt={this.props.IMDBRating} /> : <h4>No Rating Found</h4>
     } else {
       return(<h4>No Rating Found</h4>)
     }

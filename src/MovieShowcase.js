73524609167266f0e56870ebc75cca0ddf5bccd5
Richard Schaubeck <richard.schaubeck@gmail.com>
2019-03-18 18:31:15 +0000

    Automatically backed up by Learn

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -8,9 +8,9 @@ export default class MovieShowcase extends Component {
     // map over your movieData array and return the correct
     return(movieData.map(movie => {
       return(<MovieCard
-        title={movie.title}
-        IMDBRating={movie.IMDBRating}
-        genres={movie.genres}
+        title={movie.title},
+        IMDBRating={movie.IMDBRating},
+        genres={movie.genres},
         poster={movie.poster}
       />)
     })
